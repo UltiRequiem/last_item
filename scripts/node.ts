36a1cb34d8310d5662aa781c5b33ee9a17e21032
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-21 18:42:56 -0500

    :sparkles: Update

---
+++ scripts/node.ts
@@ -0,0 +1,11 @@
+import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.12/node_support.ts";
+
+buildPackage({
+  repoName: "last_item",
+  name: "@ultirequiem/last-item",
+  description: "Get the last item of an array.",
+  homepage: "https://ulti.js.org/last_item",
+  keywords: ["item", "array"],
+  license: "MIT",
+  version: "1.0.0",
+});
