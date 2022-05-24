a239ea9f49224181dd171d1cbbd54ce49822709a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-23 20:24:01 -0500

    :white_check_mark: Add Benchmarks

--- bench.ts
+++ bench.ts
@@ -4,18 +4,42 @@ import arrayLast from "https://esm.sh/array-last@1.3.0";
 
 const exampleArray = Array.from({ length: 9e7 });
 
-Deno.bench("Slice", () => {
-  exampleArray.slice(exampleArray.length - 1);
+Deno.bench("Last item [lastItem]", { group: "Single Item" }, () => {
+  lastItem(exampleArray);
 });
 
-Deno.bench("At", () => {
+Deno.bench("Last item [Array.prototype.at]", { group: "Single Item" }, () => {
   exampleArray.at(-1);
 });
 
-Deno.bench("Array Last", () => {
+Deno.bench("Last item [arrayLast]", { group: "Single Item" }, () => {
   arrayLast(exampleArray);
 });
 
-Deno.bench("Last Item", () => {
-  lastItem(exampleArray);
+Deno.bench("Last item [Direct Indexing]", { group: "Single Item" }, () => {
+  exampleArray[exampleArray.length - 1];
 });
+
+Deno.bench(
+  "Last Multiple Items [lastItem]",
+  { group: "Multiple Items" },
+  () => {
+    lastItem(exampleArray, 10);
+  },
+);
+
+Deno.bench(
+  "Last Multiple Items [Array.prototype.slice]",
+  { group: "Multiple Items" },
+  () => {
+    exampleArray.slice(10);
+  },
+);
+
+Deno.bench(
+  "Last Multiple Items [arrayLast]",
+  { group: "Multiple Items" },
+  () => {
+    arrayLast(exampleArray, 10);
+  },
+);
